9e1e10e7d74a9518493f366158d46aed4abfc3e2
ldgarber <leah.garber88@gmail.com>
2017-05-02 16:00:12 -0400

    Done.

--- stores/Store.js
+++ stores/Store.js
@@ -1,4 +1,29 @@
 'use strict';
 
 export default class Store {
+  constructor(state) {
+    this.state = state;  
+    this.listeners = []; 
+  }
+
+  getState() {
+    return this.state;  
+  }
+
+  setState(state) {
+    this.state = state;  
+    for (var listener of this.listeners) {
+      listener.call(this, state);  
+    }
+  }
+
+  addListener(listener) {
+    this.listeners.push(listener); 
+    var removeListener = () => {
+      this.listeners = this.listeners
+        .filter(l => listener !== l);  
+    }
+    return removeListener; 
+  }
+
 }
--- stores/fileStore.js
+++ stores/fileStore.js
@@ -2,9 +2,40 @@
 
 import Store from './Store';
 
-class FileStore {
+class FileStore extends Store {
+  constructor(files) {
+    super(); 
+    this.setState(files); 
+
+    this.addFile = this.addFile.bind(this); 
+    this.updateFile = this.updateFile.bind(this); 
+    this.removeFile = this.removeFile.bind(this); 
+  }
+
+  addFile() {
+    var state = this.getState().concat('');  
+    this.setState(state); 
+  }
+
+  updateFile(index, newFile) {
+    var state = this.getState().slice(0); 
+    state.splice(index, 1, newFile); 
+    this.setState(state); 
+    return; 
+  }
+
+  removeFile(index = 0) {
+    var state = this.getState(); 
+    if (state.length == 1) {
+      this.setState(['']);  
+      return; 
+    }
+    state.splice(index, 1); 
+    this.setState(state); 
+    return; 
+  }
 }
 
-const fileStore = new FileStore([]);
+const fileStore = new FileStore(['']);
 
 export default fileStore;
