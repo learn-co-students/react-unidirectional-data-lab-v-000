a5750ddb10dc7c19f9e993d09393d9a80e3e4e0c
Idayat <github email address>
2017-05-13 21:24:53 -0400

    Done.

--- stores/fileStore.js
+++ stores/fileStore.js
@@ -2,9 +2,34 @@
 
 import Store from './Store';
 
-class FileStore {
+ class FileStore extends Store {
+  // Code immediately below causes all 'fileStore' tests to fail
+  // constructor() {
+  //   super();
+  // }
+
+  updateFile (index, file) {
+    const files = this.getState().slice();
+    files.splice(index, 1, file);
+    this.setState(files);
+  }
+
+  addFile () {
+    const files = this.getState().concat('');
+    this.setState(files);
+  }
+
+  removeFile (index) {
+    if (this.state.length === 1) {
+      return this.setState(['']);
+    }
+
+    const files = this.getState().slice();
+    files.splice(index, 1);
+    this.setState(files);
+  }
 }
 
-const fileStore = new FileStore([]);
+const fileStore = new FileStore(['']);
 
 export default fileStore;
