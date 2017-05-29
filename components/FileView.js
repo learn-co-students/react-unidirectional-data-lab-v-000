830ecb0f88482e5eccc2241b89d2b39c4bb803d3
James Dolan <jwdolan820@gmail.com>
2017-05-29 04:18:47 +0000

    Done.

--- components/FileView.js
+++ components/FileView.js
@@ -1,13 +1,27 @@
+
 'use strict'
 
-import React from 'react';
-import Toolbar from './Toolbar';
+const React = require('react');
+const Toolbar = require('./Toolbar');
 
-export default class FileView extends React.Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
-}
+const FileView = ({
+  file,
+  onChange,
+  onAdd,
+  onRemove
+}) => (
+  <div className="file-view">
+    <Toolbar
+      onAdd={onAdd}
+      onRemove={onRemove}
+    />
+    <textarea
+      className="file-view__text"
+      placeholder="Type some text here..."
+      value={file}
+      onChange={onChange}
+    />
+  </div>
+);
+
+module.exports = FileView;
--- stores/fileStore.js
+++ stores/fileStore.js
@@ -1,10 +1,34 @@
+
 'use strict';
 
-import Store from './Store';
+const Store = require('./Store');
 
-class FileStore {
+class FileStore extends Store {
+  updateFile(index, file) {
+    const files = this.getState().slice();
+    files.splice(index, 1, file);
+    this.setState(files);
+  }
+
+  addFile() {
+    const files = this.getState().concat('');
+    this.setState(files);
+  }
+
+  removeFile(index) {
+    if (this.state.length === 1) {
+      return this.setState(['']);
+    }
+
+    const files = this.getState().slice()
+    files.splice(index, 1);
+    this.setState(files);
+  }
 }
 
-const fileStore = new FileStore([]);
-
-export default fileStore;
+module.exports = new FileStore([
+  'Bulbasaur',
+  'Ivysaur',
+  'Venusaur',
+  'Wartortle',
+]);
